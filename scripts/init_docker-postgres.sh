20d6af3f4342c0a7fb757a25850ebc41cbc33c23
Zdravko Donev <zdravko.donev@redis.com>
2024-12-10 18:31:02 +0200

    Update init_docker-postgres.sh

--- scripts/init_docker-postgres.sh
+++ scripts/init_docker-postgres.sh
@@ -5,6 +5,8 @@ set -e
 PGDATA=/var/lib/postgresql/data
 
 echo "wal_level = logical" >> $PGDATA/postgresql.conf
+echo "local   all             all                                     trust" > $PGDATA/pg_hba.conf
+echo "local   replication     all                                     trust" >> $PGDATA/pg_hba.conf
 echo "host    all             all             0.0.0.0/0            trust" >> $PGDATA/pg_hba.conf
 echo "host    replication             all             0.0.0.0/0            trust" >> $PGDATA/pg_hba.conf
 
