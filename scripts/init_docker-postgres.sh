1fe6539aa69dd94844ef2ff580240a79977e7ec7
Zdravko Donev <zdravko.donev@redis.com>
2024-12-10 18:22:49 +0200

    Update init_docker-postgres.sh

--- scripts/init_docker-postgres.sh
+++ scripts/init_docker-postgres.sh
@@ -5,6 +5,7 @@ set -e
 PGDATA=/var/lib/postgresql/data
 
 echo "wal_level = logical" >> $PGDATA/postgresql.conf
+echo "host    all             all             0.0.0.0/0            trust" >> $PGDATA/pg_hba.conf
 
 psql -U postgres postgres -f ~/chinook_create.sql
 psql -U postgres chinook -f ~/Chinook_PostgreSql.sql
